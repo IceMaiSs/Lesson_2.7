d35a4c5a81275e13e439470fa43c03e96118cf95
IceMaiSs <tarasevich-02@mail.ru>
2025-08-16 12:55:25 +0300

    Initial commit

---
+++ src/test/java/org/example/FactorialTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    @Test
+    void testFactorialOfZero() {
+        assertEquals(1, Factorial.factorialIterative(0));
+    }
+
+    @Test
+    void testFactorialOfOne() {
+        assertEquals(1, Factorial.factorialIterative(1));
+    }
+
+    @Test
+    void testFactorialOfSmallNumber() {
+        assertEquals(2, Factorial.factorialIterative(2));
+        assertEquals(6, Factorial.factorialIterative(3));
+        assertEquals(24, Factorial.factorialIterative(4));
+        assertEquals(120, Factorial.factorialIterative(5));
+    }
+
+    @Test
+    void testFactorialOfMediumNumber() {
+        assertEquals(720, Factorial.factorialIterative(6));
+        assertEquals(5040, Factorial.factorialIterative(7));
+        assertEquals(40320, Factorial.factorialIterative(8));
+    }
+
+    @Test
+    void testNegativeInput() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> Factorial.factorialIterative(-1));
+        assertEquals("Факториал отрицательного числа не определен", exception.getMessage());
+    }
